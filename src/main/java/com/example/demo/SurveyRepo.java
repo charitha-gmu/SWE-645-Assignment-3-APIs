0d5e1d036840054972ba532b7ccbf653584af614
Shreyas Kv <43025760+shreyas117@users.noreply.github.com>
2023-07-30 21:25:34 -0400

    Update SurveyRepo.java

--- src/main/java/com/example/demo/SurveyRepo.java
+++ src/main/java/com/example/demo/SurveyRepo.java
@@ -3,14 +3,12 @@
 // 3.Sridevi Movva – G01417871
 // 4.Shreyas Kaup Vijayananda – G01386269
 
+// SurveyRepo serves as a public interface that connects the Surveydata model file to the Amazon RDS Database, whose credentials are defined in the application.properties file
 
 package com.example.demo;
 
 
 import org.springframework.data.jpa.repository.*;
-
-
-
 
 
 public interface SurveyRepo extends JpaRepository<Surveydata, Integer> {
